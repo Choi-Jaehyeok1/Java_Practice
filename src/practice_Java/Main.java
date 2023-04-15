dc2b7001d4b60d7da94d323ebe722cc50064a886
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-15 21:28:14 +0900

    프로그래머스 java 기초 1(2정수 차)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,9 +3,9 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		//정수 num1, num2가 매개변수 주어집니다. 
-		//num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
-
+		// 정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을 
+		// return하도록 soltuion 함수를 완성해주세요.
+		
 		int a = 1;
 		int b = 2;
 		
@@ -15,7 +15,7 @@ public class Main {
 	}
 public static int solution(int num1, int num2) {
 
-	    return num1 * num2;
+	    return num1 - num2;
 	}
 	
 }
