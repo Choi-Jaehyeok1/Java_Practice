3a0a7b29ad6d0432f0b36198d4ff125319047268
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-15 21:20:35 +0900

    프로그래머스 java 기초 1(2정수 곱)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,31 +3,19 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		//등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 
-		//마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+		//정수 num1, num2가 매개변수 주어집니다. 
+		//num1과 num2를 곱한 값을 return 하도록 solution 함수를 완성해주세요.
+
+		int a = 1;
+		int b = 2;
 		
-		int common[] = {1,2,3,4};
+		solution(a, b);
 		
-		
-		
-		solution(common, common.length);
-		
+		System.out.println(solution(a, b));
 	}
-	// common_len은 배열 common의 길이입니다.
-	public static int solution(int common[], int i) {
-		
-	    int answer = 0;
-	    
-	    int d;
-	    
-	    if(common[1]-common[0] == common[2]-common[1]) {
-	    	d = common[1] = common[0];
-	    	answer = common[i-1]+d;
-	    } else {
-	    	d = common[1]/common[0];
-	    	answer = common[i-1]*d;
-	    }
-	    
-	    return answer;
+public static int solution(int num1, int num2) {
+
+	    return num1 * num2;
 	}
+	
 }
