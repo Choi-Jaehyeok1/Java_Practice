f2ca060541523f9261f404e4e85e62459b6df7a6
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-15 21:10:56 +0900

    기초 java 테스트 1 등차수열 함수

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -1,5 +1,33 @@
 package practice_Java;
 
 public class Main {
+	public static void main(String[] args) {
 
+		//등차수열 혹은 등비수열 common이 매개변수로 주어질 때, 
+		//마지막 원소 다음으로 올 숫자를 return 하도록 solution 함수를 완성해보세요.
+		
+		int common[] = {1,2,3,4};
+		
+		
+		
+		solution(common, common.length);
+		
+	}
+	// common_len은 배열 common의 길이입니다.
+	public static int solution(int common[], int i) {
+		
+	    int answer = 0;
+	    
+	    int d;
+	    
+	    if(common[1]-common[0] == common[2]-common[1]) {
+	    	d = common[1] = common[0];
+	    	answer = common[i-1]+d;
+	    } else {
+	    	d = common[1]/common[0];
+	    	answer = common[i-1]*d;
+	    }
+	    
+	    return answer;
+	}
 }
