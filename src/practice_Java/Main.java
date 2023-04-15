37be49d37d0e2cfa770a527a584b2da917e20462
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-15 21:32:25 +0900

    프로그래머스 java 기초 1(2정수 나머지)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -4,17 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 
 		// 정수 num1, num2가 매개변수로 주어질 때, 
-		// num1을 num2로 나눈 몫을 return 하도록 solution 함수를 완성해주세요.
+		// num1를 num2로 나눈 나머지를 return 하도록 solution 함수를 완성해주세요.
 		int a = 1;
 		int b = 2;
-		
-		solution(a, b);
 		
 		System.out.println(solution(a, b));
 	}
 public static int solution(int num1, int num2) {
 
-	    return num1 / num2;
+	    return num1 % num2;
 	}
 	
 }
