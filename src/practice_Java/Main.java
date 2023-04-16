a76ed62ae8a8ace0b944a67b9e9281e4b64e0c33
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-16 20:51:41 +0900

    프로그래머스 java 기초 1(2정수 크기 비교)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,18 +3,22 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 머쓱이는 40살인 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 
-		// 나이 age가 주어질 때, 2022년을 기준 출생 연도를 return 하는 solution 함수를 완성해주세요.
+		// 정수 num1과 num2가 매개변수로 주어집니다. 
+		// 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
 		
-		int age = 40;
+		int num1 = 5;
+		int num2 = 3;
 		
-		System.out.println(solution(age));
+		System.out.println(solution(num1, num2));
 	}
-public static int solution(int age) {
+public static int solution(int num1, int num2) {
 		
-		int year = 2022;
+		if(num1 == num2) {
+			return 1;
+		} else {
+			return -1;
+		}
 	
-	    return year - age + 1; // 1살을 더 더해주는 것은 우리 나이 기준이 태어난 시점에서 1살이기 때문
 	}
 	
 }
