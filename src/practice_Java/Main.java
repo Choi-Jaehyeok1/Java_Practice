947417127ca1cff20e104d16ed02116a28c1227f
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-16 21:09:13 +0900

    프로그래머스 java 기초 1(각도기 기초)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,18 +3,29 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 
-		// 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
-		
-		int num1 = 5;
-		int num2 = 3;
-		
-		System.out.println(solution(num1, num2));
+		// 각에서 0도 초과 90도 미만은 예각, 90도는 직각,
+		// 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
+		// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때
+		// 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+
+		int degrees = 60;
+
+		System.out.println(solution(degrees));
 	}
-public static int solution(int num1, int num2) {
-	
-    int answer = (num1*1000 / num2);
-    return answer;	// num1에 1000을 곱해줌에 따라 나중에 곱하는 값과 정수 값이 다르다.
+
+	public static int solution(int degrees) {
+
+		if(0 < degrees && degrees < 90) {
+			return 1;
+		} else if(degrees == 90) {
+			return 2;
+		} else if(90 < degrees && degrees < 180) {
+			return 3;
+		} else if(degrees == 180) {
+			return 4;
+		} else {
+			return 0;
+		}
 	}
-	
+
 }
