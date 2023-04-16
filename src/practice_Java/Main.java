f3ee4f1b05830e355dc6c5bbd76e72c880074b69
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-16 21:13:38 +0900

    프로그래머스 java 기초 1(n정수 이하의 짝수들의 합)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,29 +3,23 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 각에서 0도 초과 90도 미만은 예각, 90도는 직각,
-		// 90도 초과 180도 미만은 둔각 180도는 평각으로 분류합니다.
-		// 각 angle이 매개변수로 주어질 때 예각일 때 1, 직각일 때 2, 둔각일 때
-		// 3, 평각일 때 4를 return하도록 solution 함수를 완성해주세요.
+		// 정수 n이 주어질 때, n이하의 짝수를 모두 
+		// 더한 값을 return 하도록 solution 함수를 작성해주세요.
 
-		int degrees = 60;
+		int num = 10;
 
-		System.out.println(solution(degrees));
+		System.out.println(solution(num));
 	}
 
-	public static int solution(int degrees) {
-
-		if(0 < degrees && degrees < 90) {
-			return 1;
-		} else if(degrees == 90) {
-			return 2;
-		} else if(90 < degrees && degrees < 180) {
-			return 3;
-		} else if(degrees == 180) {
-			return 4;
-		} else {
-			return 0;
+	public static int solution(int num) {
+		int sum = 0;
+		
+		for(int i = 0; i <= num; i++) {
+			if(i % 2 == 0) {
+				sum = sum +i;
+			}
 		}
+		return sum;
 	}
 
 }
