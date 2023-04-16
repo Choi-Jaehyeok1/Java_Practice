d54130ed5bbbd9b0c13114b5d28bd4777861ec61
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-16 21:00:51 +0900

    프로그래머스 java 기초 1(2정수의 나눠서 *1000 한 값의 정수값만 출력하기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,8 +3,8 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 num1과 num2가 주어질 때, 
-		// num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
+		// 정수 num1과 num2가 매개변수로 주어질 때, num1을 num2로 나눈 값에 1,000을 곱한 후 
+		// 정수 부분을 return 하도록 soltuion 함수를 완성해주세요.
 		
 		int num1 = 5;
 		int num2 = 3;
@@ -13,8 +13,8 @@ public class Main {
 	}
 public static int solution(int num1, int num2) {
 	
-			return num1 + num2;
-	
+    int answer = (num1*1000 / num2);
+    return answer;	// num1에 1000을 곱해줌에 따라 나중에 곱하는 값과 정수 값이 다르다.
 	}
 	
 }
