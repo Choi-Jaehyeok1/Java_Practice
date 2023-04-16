858408c12996d4293cf39ee7bb42750059b4c6b2
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-16 20:54:24 +0900

    프로그래머스 java 기초 1(2정수의 합)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,8 +3,8 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 num1과 num2가 매개변수로 주어집니다. 
-		// 두 수가 같으면 1 다르면 -1을 retrun하도록 solution 함수를 완성해주세요.
+		// 정수 num1과 num2가 주어질 때, 
+		// num1과 num2의 합을 return하도록 soltuion 함수를 완성해주세요.
 		
 		int num1 = 5;
 		int num2 = 3;
@@ -12,12 +12,8 @@ public class Main {
 		System.out.println(solution(num1, num2));
 	}
 public static int solution(int num1, int num2) {
-		
-		if(num1 == num2) {
-			return 1;
-		} else {
-			return -1;
-		}
+	
+			return num1 + num2;
 	
 	}
 	
