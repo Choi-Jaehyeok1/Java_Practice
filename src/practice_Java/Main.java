a697d1c23c7429da140f381e9802b33333204900
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-19 16:00:39 +0900

    프로그래머스 java 기초 1(중복된 숫자 개수 count)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,29 +3,25 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 배열 numbers가 매개변수로 주어집니다. 
-		// numbers의 원소의 평균값을 return하도록 solution 함수를 완성해주세요.
+		// 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
+		// array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
 
-		// numbers_len은 배열 numbers의 길이입니다.
+		int[] arr = {1, 1, 2, 2, 1, 3, 4, 1};
+		int n = 1;
 		
-		
-		
-		int [] num = {89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99};
-		
-		
-		System.out.println(solution(num, num.length));
+		System.out.println(solution(arr, n));
 	}
 
-	public static double solution(int num[], int numbers_len) {
-	    double answer = 0;
-	    double sum = 0;
-	    
-	    for(int i = 0; i < numbers_len ; i++) {
-	    	sum = sum + num[i];
-	    }
-	    answer = sum / numbers_len ;
-	    
-	    return answer; 
+	public static int solution(int[] arr, int n) {
+		int count = 0;
+		
+		for(int i = 0; i < arr.length; i++) {
+			if(arr[i] == n) {
+				count ++;
+			}
+		}
+		;
+		return count;
 	}
 
 }
