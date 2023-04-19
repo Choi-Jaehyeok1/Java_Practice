a4239c0454ef4943d34d5ef9a0d638d0f97aa8a3
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-19 16:42:45 +0900

    프로그래머스 java 기초 1(배열 글자 원소 개수 count)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,25 +3,21 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수가 담긴 배열 array와 정수 n이 매개변수로 주어질 때,
-		// array에 n이 몇 개 있는 지를 return 하도록 solution 함수를 완성해보세요.
+		// 문자열 배열 strlist가 매개변수로 주어집니다. 
+		// strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
 
-		int[] arr = {1, 1, 2, 2, 1, 3, 4, 1};
-		int n = 1;
-		
-		System.out.println(solution(arr, n));
+		String[] arr = {"I", "Love", "Programmers"};
+		solution(arr);
+
 	}
 
-	public static int solution(int[] arr, int n) {
-		int count = 0;
-		
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] == n) {
-				count ++;
-			}
-		}
-		;
-		return count;
-	}
-
+	public static int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        
+        for(int i = 0; i< strlist.length; i++) {
+        	answer[i] = strlist[i].length();
+        }
+        
+        return answer;
+    }
 }
