ec109d4e3c8be5ae831c94a078e1818bb0300cb6
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-20 16:36:44 +0900

    프로그래머스 java 기초 1(배열 거꾸로 저장)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,19 +3,26 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 문자열 배열 strlist가 매개변수로 주어집니다. 
-		// strlist 각 원소의 길이를 담은 배열을 retrun하도록 solution 함수를 완성해주세요.
+		// 정수가 들어 있는 배열 num_list가 매개변수로 주어집니다. 
+		// num_list의 원소의 순서를 거꾸로 뒤집은 배열을 return하도록 
+		// solution 함수를 완성해주세요.
+		
+		
+		
+		int[] num_list = {1, 2, 3, 4, 5};
+		int[] num_return = solution(num_list);
 
-		String[] arr = {"I", "Love", "Programmers"};
-		solution(arr);
-
+		for(int i = 0; i < num_return.length; i++) {
+			System.out.println(num_return[i]);
+		}
+		
 	}
 
-	public static int[] solution(String[] strlist) {
-        int[] answer = new int[strlist.length];
+	public static int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
         
-        for(int i = 0; i< strlist.length; i++) {
-        	answer[i] = strlist[i].length();
+        for(int i = num_list.length -1 ; i >= 0; i--) {
+        	answer[num_list.length - 1 - i] = num_list[i];
         }
         
         return answer;
