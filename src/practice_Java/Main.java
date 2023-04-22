63da4c9cf29f4e4010064295e0bf58288d1560d7
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-22 23:06:05 +0900

    프로그래머스 java 기초 1(좌표 선정 코드)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,20 +3,32 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 문자열 my_string이 매개변수로 주어집니다. 
-		// my_string을 거꾸로 뒤집은 문자열을 return하도록 
+		// 사분면은 한 평면을 x축과 y축을 기준으로 나눈 네 부분입니다. 
+		// 사분면은 아래와 같이 1부터 4까지 번호를매깁니다.
+
+		// x 좌표와 y 좌표가 모두 양수이면 제1사분면에 속합니다.
+		// x 좌표가 음수, y 좌표가 양수이면 제2사분면에 속합니다.
+		// x 좌표와 y 좌표가 모두 음수이면 제3사분면에 속합니다.
+		// x 좌표가 양수, y 좌표가 음수이면 제4사분면에 속합니다.
+		// x 좌표 (x, y)를 차례대로 담은 정수 배열 dot이 매개변수로 주어집니다. 
+		// 좌표 dot이 사분면 중 어디에 속하는지 1, 2, 3, 4 중 하나를 return 하도록 
 		// solution 함수를 완성해주세요.
 		
-		String my_string = "love";
-		String return_string = solution(my_string);
-		System.out.println(return_string);
+		int[] dot = {1,-2};
+		int return_space = solution(dot);
+		System.out.println(return_space);
 	}
 
-	    public static String solution(String my_string) {
-	        String answer = "";
-	        for(int i = my_string.length() -1 ; i >= 0; i--) {
-	        	answer = answer + my_string.charAt(i);
+	    public static int solution(int[] dot) {
+	        if(dot[0] > 0 && dot[1] > 0) {
+	        	return 1;
+	        } else if(dot[0] < 0 && dot[1] > 0) {
+	        	return 2;
+	        } else if(dot[0] < 0 && dot[1] < 0) {
+	        	return 3;
+	        } else {
+	        	return 4;
 	        }
-	        return answer;
+	        
 	    }
 }
