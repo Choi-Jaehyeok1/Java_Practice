41449e63c477c9051e857a7ee5e7fd92f0201905
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-24 16:12:13 +0900

    프로그래머스 java 기초 1(옷 가게 할인 코드 만들기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,26 +3,29 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 머쓱이는 추운 날에도 아이스 아메리카노만 마십니다. 
-		// 아이스 아메리카노는 한잔에 5,500원입니다. 
-		// 머쓱이가 가지고 있는 돈 money가 매개변수로 주어질 때, 
-		// 머쓱이가 최대로 마실 수 있는 아메리카노의 잔 수와 남는 
-		// 돈을 순서대로 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+		// 머쓱이네 옷가게는 10만 원 이상 사면 5%, 
+		// 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
+		// 구매한 옷의 가격 price가 주어질 때, 
+		// 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
 
-		int money = 10000;
-		int[] return_money = solution(money);
-		System.out.println(return_money);
+		int price = 1000000;
+		int discount = solution(price);
+		System.out.println(discount);
 	}
 
 
-	public static int[] solution(int money) {
-        int[] answer = new int [2];
+	public static int solution(int price) {
+        int answer = 0;
         
-        int coffee = 5500;
-        
-        answer[0] = (int) (money / coffee);
-        answer[1] = (int) (money % coffee);
-        
+        if(100000 <= price  && price < 300000) {
+        	answer = (int) (price * 0.95);
+        } else if(300000 <= price  && price < 500000) {
+        	answer = (int) (price * 0.90);
+        } else if(500000 <= price) {
+        	answer = (int) (price * 0.80);
+        } else {
+        	answer = price;
+        }
         
         return answer;
     }
