ee0cb0e7e1dc3c0ad5145a1ba981b3cd104e01c2
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-25 16:24:14 +0900

    프로그래머스 java 기초 1(정수의 순서쌍의 곱으로 정수 값 count_수정 완료)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,31 +3,29 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어 
-		// 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로 
-		// 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를 
+		// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어
+		// 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로
+		// 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를
 		// return하도록 solution 함수를 완성해주세요.
 
-		int n = 100;
+		int n = 120;
 		int return_answer = solution(n);
 		System.out.println(return_answer);
 
 	}
 
 	public static int solution(int n) {
-        int answer = 0;
-        
-        for(int i = 1; i <= n; i++) {
-        	for(int j = 1; j <= n; j++) {
+		int answer = 0;
 
-        		if(i * j == n) {
-        			answer ++;
-        		}
-        	}
-        }
-        
-        return answer;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				answer++;
+			}
+		}
+
+		return answer;
 
 	}
+
 
 }
