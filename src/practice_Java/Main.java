8c793d2b930c96d56e20e36fab167d21e0311971
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-25 16:29:29 +0900

    프로그래머스 java 기초 1(글씨 크기 구하기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,28 +3,20 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 순서쌍이란 두 개의 숫자를 순서를 정하여 짝지어
-		// 나타낸 쌍으로 (a, b)로 표기합니다. 자연수 n이 매개변수로
-		// 주어질 때 두 숫자의 곱이 n인 자연수 순서쌍의 개수를
-		// return하도록 solution 함수를 완성해주세요.
+		// 머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 
+		// 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 
+		// 적으려고 하며, 편지를 가로로만 적을 때, 
+		// 축하 문구 message를 적기 위해 필요한 편지지의 최소 
+		// 가로길이를 return 하도록 solution 함수를 완성해주세요.
 
-		int n = 120;
-		int return_answer = solution(n);
+		String message = "happy birthday!";
+		int return_answer = solution(message);
 		System.out.println(return_answer);
 
 	}
 
-	public static int solution(int n) {
-		int answer = 0;
-
-		for (int i = 1; i <= n; i++) {
-			if (n % i == 0) {
-				answer++;
-			}
-		}
-
-		return answer;
-
+	public static int solution(String message) {
+        return message.length()*2;
 	}
 
 
