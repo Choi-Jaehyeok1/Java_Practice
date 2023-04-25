7aa0777f67811edcc8d7f0d938c707c40cf66a9c
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-25 14:35:08 +0900

    프로그래머스 java 기초 1(중앙값 구하기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -1,32 +1,27 @@
 package practice_Java;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 
-		// 머쓱이네 옷가게는 10만 원 이상 사면 5%, 
-		// 30만 원 이상 사면 10%, 50만 원 이상 사면 20%를 할인해줍니다.
-		// 구매한 옷의 가격 price가 주어질 때, 
-		// 지불해야 할 금액을 return 하도록 solution 함수를 완성해보세요.
+		// 중앙값은 어떤 주어진 값들을 크기의 순서대로 
+		// 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 
+		// 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 
+		// 정수 배열 array가 매개변수로 주어질 때, 중앙값을 
+		// return 하도록 solution 함수를 완성해보세요.
 
-		int price = 1000000;
-		int discount = solution(price);
-		System.out.println(discount);
+		int[] array = {1,2,7,9,10};
+		int midpoint = solution(array);
+		System.out.println(midpoint);
 	}
 
 
-	public static int solution(int price) {
+	public static int solution(int[] array) {
         int answer = 0;
         
-        if(100000 <= price  && price < 300000) {
-        	answer = (int) (price * 0.95);
-        } else if(300000 <= price  && price < 500000) {
-        	answer = (int) (price * 0.90);
-        } else if(500000 <= price) {
-        	answer = (int) (price * 0.80);
-        } else {
-        	answer = price;
-        }
-        
+        Arrays.sort(array);
+        answer = array[array.length/2];
         return answer;
     }
 
