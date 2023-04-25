2e8b7ff0845f0eed4f421ddbc793c71f466e0246
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-25 15:15:47 +0900

    프로그래머스 java 기초 1(반복 글 만들기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -5,23 +5,29 @@ import java.util.Arrays;
 public class Main {
 	public static void main(String[] args) {
 
-		// 중앙값은 어떤 주어진 값들을 크기의 순서대로 
-		// 정렬했을 때 가장 중앙에 위치하는 값을 의미합니다. 
-		// 예를 들어 1, 2, 7, 10, 11의 중앙값은 7입니다. 
-		// 정수 배열 array가 매개변수로 주어질 때, 중앙값을 
+		// 문자열 my_string과 정수 n이 매개변수로 주어질 때, 
+		// my_string에 들어있는 각 문자를 n만큼 반복한 문자열을 
 		// return 하도록 solution 함수를 완성해보세요.
 
-		int[] array = {1,2,7,9,10};
-		int midpoint = solution(array);
-		System.out.println(midpoint);
+
+		String my_string = "love";
+		int n = 3;
+		String return_answer = solution(my_string, n);
+		System.out.println(return_answer);
+		
 	}
 
 
-	public static int solution(int[] array) {
-        int answer = 0;
+	public static String solution(String my_string, int n) {
+        String answer = "";
+        String[] array_my_string = my_string.split("");
+     
+		for(int i = 0; i < array_my_string.length; i++) {
+			for(int j = 0; j < n; j++) {
+				answer = answer + array_my_string[i];
+			}
+		}
         
-        Arrays.sort(array);
-        answer = array[array.length/2];
         return answer;
     }
 
