5aa09103de62e5d887dc83388409fd172073e614
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-25 16:59:00 +0900

    프로그래머스 java 기초 1(모음제거하기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,21 +3,30 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 머쓱이는 할머니께 생신 축하 편지를 쓰려고 합니다. 
-		// 할머니가 보시기 편하도록 글자 한 자 한 자를 가로 2cm 크기로 
-		// 적으려고 하며, 편지를 가로로만 적을 때, 
-		// 축하 문구 message를 적기 위해 필요한 편지지의 최소 
-		// 가로길이를 return 하도록 solution 함수를 완성해주세요.
+		// 영어에선 a, e, i, o, u 다섯 가지 알파벳을 모음으로 분류합니다.
+		// 문자열 my_string이 매개변수로 주어질 때
+		// 모음을 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
 
-		String message = "happy birthday!";
-		int return_answer = solution(message);
+		String my_string = "bus";
+		String return_answer = solution(my_string);
 		System.out.println(return_answer);
 
 	}
 
-	public static int solution(String message) {
-        return message.length()*2;
+	public static String solution(String my_string) {
+		String answer = "";
+        String[] replace = {"a", "e", "i", "o", "u"};
+        for(int i = 0; i<replace.length; i++) {
+			if( my_string.contains(replace[i])) {
+				answer = my_string.replaceAll(replace[i], "");
+				my_string = answer;
+			} else {
+                answer =my_string;
+            }
+		}
+        
+        
+        return answer;
 	}
-
 
 }
