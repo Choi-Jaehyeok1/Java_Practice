6ecade33268c1fa9b62cb73322e43861c5984ffc
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-26 16:00:03 +0900

    프로그래머스 java 기초 1(자리수의 합)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -1,40 +1,30 @@
 package practice_Java;
 
-import java.util.Arrays;
-
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 n이 매개변수로 주어질 때, 
-		// n 이하의 홀수가 오름차순으로 담긴 배열을 
-		// return하도록 solution 함수를 완성해주세요.
+		// 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 
+		// return하도록 solution 함수를 완성해주세요
 
-		int n = 15;
-		int[] return_answer = solution(n);
-//		System.out.println(return_answer);
-		for(int i = 0; i < return_answer.length; i++) {
-			System.out.println(return_answer[i]);
-		}
+		int n = 1234;
+		int return_answer = solution(n);
+		System.out.println(return_answer);
+//		for(int i = 0; i < return_answer.length; i++) {
+//			System.out.println(return_answer[i]);
+//		}
 	}
 
-	public static int[] solution(int n) {
-		int a;
-		if(n % 2 == 0) {
-			a = n/2;
-		} else {
-			a = n/2 + 1;
-		}
-		
-        int[] answer = new int [a];
-        int count = 0;
-        for(int i = 1; i <= n; i++) {
-        	if(i%2 != 0) {
-        		answer[count] = i;
-        		count++;
-        	}
+	public static int solution(int n) {
+        int answer = 0;
+        String n2 = Integer.toString(n);
+        int[] n3 = new int [n2.length()];
+        for(int i = 0; i < n2.length(); i++) {
+        	n3[i] = n2.charAt(i) - '0';
         }
-        Arrays.sort(answer);
         
+        for(int i = 0; i< n3.length; i++) {
+        	answer = answer + n3[i];
+        }
         return answer;
 	}
 
