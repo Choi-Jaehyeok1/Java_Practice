958917325f233b19db62c3af227379d8382e03d8
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-27 17:04:08 +0900

    프로그래머스 java 기초 1(문장에서 숫자만 추출하여 합구하기)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,28 +3,24 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 정수 n이 매개변수로 주어질 때 n의 각 자리 숫자의 합을 
-		// return하도록 solution 함수를 완성해주세요
+		// 문자열 my_string이 매개변수로 주어집니다. 
+		// my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
 
-		int n = 1234;
-		int return_answer = solution(n);
+		String my_string = "aAb1B2cC34oOp";
+		int return_answer = solution(my_string);
 		System.out.println(return_answer);
 //		for(int i = 0; i < return_answer.length; i++) {
 //			System.out.println(return_answer[i]);
 //		}
 	}
 
-	public static int solution(int n) {
+	public static int solution(String my_string) {
         int answer = 0;
-        String n2 = Integer.toString(n);
-        int[] n3 = new int [n2.length()];
-        for(int i = 0; i < n2.length(); i++) {
-        	n3[i] = n2.charAt(i) - '0';
+        String[] change = my_string.replaceAll("[^\\d]", "").split("");
+        for(int i = 0; i < change.length; i++) {
+        	answer = answer + Integer.parseInt(change[i]);
         }
         
-        for(int i = 0; i< n3.length; i++) {
-        	answer = answer + n3[i];
-        }
         return answer;
 	}
 
