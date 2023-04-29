e9886063c64917f3b5a4ef8c18aefaf6a0c8ab54
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-04-29 22:06:57 +0900

    프로그래머스 java 기초 1(포함문장 여부 확인)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -3,22 +3,26 @@ package practice_Java;
 public class Main {
 	public static void main(String[] args) {
 
-		// 문자열 my_string이 매개변수로 주어집니다. 
-		// my_string안의 모든 자연수들의 합을 return하도록 solution 함수를 완성해주세요.
+		// 문자열 str1, str2가 매개변수로 주어집니다. 
+		// str1 안에 str2가 있다면 1을 없다면 2를 
+		// return하도록 solution 함수를 완성해주세요.
 
-		String my_string = "aAb1B2cC34oOp";
-		int return_answer = solution(my_string);
+		String str1 = "ab6CDE443fgh22iJKlmn1o";
+		String str2 = "6CD";
+		int return_answer = solution(str1, str2);
 		System.out.println(return_answer);
 //		for(int i = 0; i < return_answer.length; i++) {
 //			System.out.println(return_answer[i]);
 //		}
 	}
 
-	public static int solution(String my_string) {
+	public static int solution(String str1, String str2) {
         int answer = 0;
-        String[] change = my_string.replaceAll("[^\\d]", "").split("");
-        for(int i = 0; i < change.length; i++) {
-        	answer = answer + Integer.parseInt(change[i]);
+        
+        if(str1.contains(str2)) {
+        	answer = 1;
+        } else {
+        	answer =2;
         }
         
         return answer;
