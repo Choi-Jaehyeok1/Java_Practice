19f43eef219c68a2311c6c9d792dadbea16b25d0
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-05-03 16:20:36 +0900

    프로그래머스 java 기초 1(가위바위보)

--- src/practice_Java/Main.java
+++ src/practice_Java/Main.java
@@ -1,29 +1,33 @@
 package practice_Java;
 
+import java.util.Arrays;
+
 public class Main {
 	public static void main(String[] args) {
 
-		// 어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 
-		// 처음 세균의 마리수 n과 경과한 시간 t가 매개변수로 주어질 때 
-		// t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
-		
+		// 가위는 2 바위는 0 보는 5로 표현합니다.
+		// 가위 바위 보를 내는 순서대로 나타낸 문자열
+		// rsp가 매개변수로 주어질 때, rsp에 저장된 가위 바위 보를
+		// 모두 이기는 경우를 순서대로 나타낸 문자열을 return하도록 solution 함수를 완성해보세요.
 
-		
-		int n = 100;
-		int t = 3;
-		int return_answer = solution(n,  t);
+		String rsp = "502";
+		String return_answer = solution(rsp);
 		System.out.println(return_answer);
 	}
 
-	public static int solution(int n, int t) {
-        int answer = 0;
-        int n2 = 1;
-        for(int i = 1; i <= t; i++) {
-        	n2 = n2*2;
-        }
-        answer = n * n2;
-        
-        return answer;
+	public static String solution(String rsp) {
+		String answer = "";
+		for(int i = 0; i<rsp.length(); i++) {
+			if(rsp.split("")[i].equals("0")) {
+				answer += "5";
+			} else if(rsp.split("")[i].equals("5")) {
+				answer += "2";
+			} else {
+				answer += "0";
+			}
+		}
+
+		return answer;
 	}
 
 }
