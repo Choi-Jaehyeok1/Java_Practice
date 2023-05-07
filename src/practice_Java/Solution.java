36a37b1f58db802699b345fef6bab576a5b74a32
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-05-07 23:15:26 +0900

    프로그래머스 java 기초 1(두 수의 곱 최댓값 구하기)

--- src/practice_Java/Solution.java
+++ src/practice_Java/Solution.java
@@ -1,27 +1,36 @@
 package practice_Java;
 
-import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Solution {
-//	public static void main(String[] args) {
-
-		// "*"의 높이와 너비를 1이라고 했을 때, "*"을 이용해 직각 이등변 삼각형을 그리려고합니다. 
-		// 정수 n 이 주어지면 높이와 너비가 n 인 직각 이등변 삼각형을 출력하도록 코드를 작성해보세요.
-
-//		String my_string = "hi12392";
-//		solution(my_string);
-//	}
-
 	public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String y = "*";
-        for(int i = 0; i< n; i ++) {
-        	System.out.println(y);
-        	y = y + "*";
-        }
 
-        System.out.println(n);
+		// 정수 배열 numbers가 매개변수로 주어집니다. 
+		// numbers의 원소 중 두 개를 곱해 만들 수 있는 
+		// 최댓값을 return하도록 solution 함수를 완성해주세요.
+
+		int[] numbers = {1,2,-3,4,-5};
+		int re_answer = solution(numbers);
+		System.out.println(re_answer);
+	}
+
+	public static int solution(int[] numbers) {
+        int answer = 0;
+        ArrayList<Integer> num = new ArrayList<Integer>();
+        
+        for(int i = 0; i< numbers.length; i++) {
+        	for(int j = i+1; j< numbers.length; j ++) {
+        		num.add(numbers[i]*numbers[j]);
+        	}
+        }
+        answer = Collections.max(num);
+        
+        return answer;
+        
+        
+        
+
 	}
 
 }
