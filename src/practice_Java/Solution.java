d1cd254bd0fda39e7714f6a8fe6cb066d9d5c116
Jaehyeok Choi <jaehyeok3245@gmail.com>
2023-05-12 19:09:22 +0900

    프로그래머스 java 기초 1(문자열 바꾸기)

--- src/practice_Java/Solution.java
+++ src/practice_Java/Solution.java
@@ -3,36 +3,31 @@ package practice_Java;
 public class Solution {
 	public static void main(String[] args) {
 
-		// 정수가 담긴 배열 numbers와 문자열 direction가 매개변수로 주어집니다. 
-		// 배열 numbers의 원소를 direction방향으로 한 칸씩 회전시킨 배열을 
-		// return하도록 solution 함수를 완성해주세요.
+		// 문자열 my_string과 정수 num1, num2가 매개변수로 주어질 때, 
+		// my_string에서 인덱스 num1과 인덱스 num2에 해당하는 문자를 바꾼 문자열을 
+		// return 하도록 solution 함수를 완성해보세요.
 
-
-		
-		int[] numbers = {4, 455, 6, 4, -1, 45, 6};
-		String direction = "left";
-		 int[] re_answer = solution(numbers, direction);
-//		System.out.println(re_answer);
-		
-		for(int i = 0; i<re_answer.length; i++) {
-			System.out.println(re_answer[i]);
-		}
-		
+		String my_string = "hello";
+		int num1 = 1;
+		int num2 = 2;
+		String re_answer = solution(my_string, num1, num2);
+		System.out.println(re_answer);
+//		for(int i = 0; i<re_answer.length; i++) {
+//			System.out.println(re_answer[i]);
+//		}
 		
 	}
 
-	public static int[] solution(int[] numbers, String direction) {
-        int[] answer = new int [numbers.length];
-        if(direction.equals("right")) {
-        	for(int i = 0 ; i < numbers.length-1; i++) {
-        		answer[i+1] = numbers[i];
-        	}
-        	answer[0] = numbers[numbers.length-1];
-        } else {
-        	for(int i = 1 ; i < numbers.length; i++) {
-        		answer[i-1] = numbers[i];
-        	}
-        	answer[numbers.length-1] = numbers[0];
+	public static String solution(String my_string, int num1, int num2) {
+        String answer = "";
+        String [] re_string = my_string.split("");
+        String a = null;
+        a = re_string[num1];
+        re_string[num1] = re_string[num2];
+        re_string[num2] = a;
+        
+        for(int i = 0; i<re_string.length; i++) {
+        	answer += re_string[i];
         }
         
         return answer;
